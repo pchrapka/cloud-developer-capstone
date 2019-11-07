df46cb637b9873e7c834c8c0ccfd64e9351245a6
Phil Chrapka <pchrapka@gmail.com>
2019-11-07 18:48:57 +0000

    split out data layer and business logic

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,20 +1,15 @@
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
-import * as uuid from 'uuid'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { TodoItem } from '../../models/TodoItem'
 import { getUserId } from '../utils'
 
 import { createLogger } from '../../utils/logger'
+import { createTodo } from '../../businessLogic/todos'
 const logger = createLogger('createTodo')
-
-const docClient = new AWS.DynamoDB.DocumentClient()
-const todosTable = process.env.TODOS_TABLE
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
@@ -43,29 +38,3 @@ handler.use(
     credentials: true
   })
 )
-
-export async function createTodo(
-  createTodoRequest: CreateTodoRequest,
-  userId: string
-): Promise<TodoItem> {
-
-  const todoId = uuid.v4()
-
-  const todoItem = {
-    todoId: todoId,
-    userId: userId,
-    name: createTodoRequest.name,
-    dueDate: createTodoRequest.dueDate,
-    createdAt: new Date().toISOString(),
-    done: false
-  }
-
-  logger.info('Creating new todo', todoItem)
-
-  await docClient.put({
-    TableName: todosTable,
-    Item: todoItem
-  }).promise()
-
-  return todoItem as TodoItem
-}
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,5 +1,4 @@
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
@@ -9,11 +8,8 @@ import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
 import { createLogger } from '../../utils/logger'
 import { getUserId } from '../utils'
+import { updateTodo } from '../../businessLogic/todos'
 const logger = createLogger('updateTodo')
-
-const docClient = new AWS.DynamoDB.DocumentClient()
-const todosTable = process.env.TODOS_TABLE
-const todosIndex = process.env.TODOS_INDEX
 
 export const handler = middy( async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.info('Event', event)
@@ -22,11 +18,8 @@ export const handler = middy( async (event: APIGatewayProxyEvent): Promise<APIGa
   const userId = getUserId(event)
 
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  logger.info({userId: userId, todoId: todoId})
   try{
     await updateTodo(userId, todoId, updateTodoRequest)
-    logger.info('Updated todo')
-
     return {
       statusCode: 200,
       body: ''
@@ -45,47 +38,3 @@ handler.use(
     credentials: true
   })
 )
-
-async function updateTodo(
-  userId: string,
-  todoId: string,
-  updatedTodoRequest: UpdateTodoRequest
-) {
-
-  const result = await docClient.query({
-    TableName: todosTable,
-    IndexName: todosIndex,
-    KeyConditionExpression: 'userId = :userId and todoId = :todoId',
-    ExpressionAttributeValues:{
-      ':userId': userId,
-      ':todoId': todoId
-    }
-  }).promise()
-
-  if (result.Count === 0){
-    logger.error('Todo not found')
-    throw new Error('Todo not found')
-  }
-
-  const todoItem = result.Items[0]
-
-  logger.info('Updating todo')
-
-  await docClient.update({
-    TableName: todosTable,
-    Key:{
-      "userId": todoItem.userId,
-      "createdAt": todoItem.createdAt
-    },
-    UpdateExpression: "set done=:done, dueDate=:dueDate, #name=:name",
-    ExpressionAttributeValues:{
-        ":done":updatedTodoRequest.done,
-        ":dueDate":updatedTodoRequest.dueDate,
-        ":name":updatedTodoRequest.name,
-    },
-    ExpressionAttributeNames: {
-      "#name": "name"
-    },
-    ReturnValues:"UPDATED_NEW"
-  }).promise()
-}
